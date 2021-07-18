8ddf619cb41c5300037c8fc998e2afa91abd2c78
Illia Kononenko <ilyanes88@gmail.com>
2021-07-18 13:14:13 +0300

    task3

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,7 @@
+// Напиши скрипт, который для каждого элемента массива ingredients создаст
+// отдельный li, после чего вставит все li за одну операцию в список ul.ingredients.
+// Для создания DOM - узлов используй document.createElement().
+
 const ingredients = [
   "Картошка",
   "Грибы",
@@ -6,3 +10,15 @@ const ingredients = [
   "Зелень",
   "Приправы",
 ];
+console.dir(ingredients);
+const ingredientsList = document.querySelector("#ingredients");
+// console.log(ingredientsList);
+
+const ingredientsListNew = ingredients.map((ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingredient;
+  return ingredientItem;
+});
+// console.log(ingredientsListNew);
+ingredientsList.append(...ingredientsListNew);
+console.log(ingredientsList);
--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,7 @@
+// Используй массив объектов images для создания тегов img вложенных в li.
+// Для создания разметки используй шаблонные строки и insertAdjacentHTML().
+// Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+// Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
 const images = [
   {
     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
@@ -12,3 +16,11 @@ const images = [
     alt: "Group of Horses Running",
   },
 ];
+const gallery = document.querySelector("#gallery");
+console.log(gallery);
+
+const galleryList = images.reduce((acc, { url, alt }) => {
+  return acc + `<li><img src= ${url} alt='${alt}'/></li>`;
+}, "");
+
+gallery.insertAdjacentHTML("afterbegin", galleryList);
